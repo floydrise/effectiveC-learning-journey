c4ac4e6fc619f67fe9311aea6523923525563eac
Stefan <floydrise@gmail.com>
2025-04-30 20:27:18 +0300

    Add better example
    Better example and description of what's happening

--- chapter-7/copy_str.c
+++ chapter-7/copy_str.c
@@ -3,11 +3,14 @@
 #include <string.h>
 
 int main(void) {
-  char *str = "howdy";
-  char *str_cpy = strdup(str);
+  const char *str = "howdy"; // static global string literal
+  char str_arr[6] = "howdy"; // stack allocated char array
+  printf("%s\n", str_arr);
+  char *str_cpy = strdup(str); // heap allocated copy of the string literal, shorthand for malloc() + strcpy()
   if (str_cpy != NULL) {
     printf("%s\n", str_cpy);
     free(str_cpy);
+    str_cpy = NULL;
   };
   return EXIT_SUCCESS;
 };
